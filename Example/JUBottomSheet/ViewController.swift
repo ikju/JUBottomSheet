c06c31f1b4345162c7eff928c2b5546f6e718424
JudeSong <ikjus1@naver.com>
2021-03-31 23:13:01 +0900

    3/31

--- Example/JUBottomSheet/ViewController.swift
+++ Example/JUBottomSheet/ViewController.swift
@@ -20,5 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionAlert(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+                    JUBottomSheet(title: "와 이게 되네", confirm: "확인버튼도 커스텀할 수 있네") {
+                        print("completion handler 까지!?!")
+                    }.show(in: self.view)
+                } else {
+                    // Fallback on earlier versions
+                }
+    }
 }
 
